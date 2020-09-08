0744f6cedabb34afdd0f8fbfa32ba4eeb3c5c3d5
Michael SALIHI <admin@prestance-informatique.fr>
2020-09-08 14:20:59 +0200

    Some refactoring for better composition

--- src/jsoup_scraping/core.clj
+++ src/jsoup_scraping/core.clj
@@ -5,26 +5,49 @@
             [clojure.java.io :as io])
   (:import org.jsoup.Jsoup))
 
-(def URL "http://www.newegg.com/Product/ProductList.aspx?Submit=ENE&N=-1&IsNodeId=1&Description=GTX&bop=And&Page=1&PageSize=36&order=BESTMATCH")
+(def site-url "http://www.newegg.com/Product/ProductList.aspx?Submit=ENE&N=-1&IsNodeId=1&Description=GTX&bop=And&Page=1&PageSize=36&order=BESTMATCH")
 
-(defn get-page []
-  (.get (Jsoup/connect URL)))
+(defn fetch-page [url]
+  (.get (Jsoup/connect url)))
 
-(defn get-elems [page css]
-  (.select page css))
+(defn get-text [root selector]
+  (-> (.select root selector)
+      .text))
+
+(defn get-attr [root selector attribute]
+  (-> (.select root selector)
+      (.attr attribute)))
+
+(defn parse-product [item]
+  (let [product-name (-> (get-text item ".item-title")
+                         (str/replace "," "|"))
+        brand        (get-attr item "div.item-branding > a > img" "title")
+        shipping     (-> (.text (.select item "div.item-action ul.price > li.price-ship"))
+                         (str/replace "$" "")
+                         (str/replace "Shipping" ""))]
+    [brand product-name shipping]))
+
+(defn generate-csv [filename url]
+  (let [html (fetch-page url)
+        csv-rows (map parse-product (.select html "div.item-container"))
+        headers ["brand" "product_name" "shipping"]]
+    (with-open [writer (io/writer filename)]
+      (csv/write-csv writer
+                     (concat [headers]
+                             csv-rows)))))
 
 (defn -main []
-  (let [html (get-page)
-        elems (get-elems html "div.item-container")
-        headers ["brand" "product_name" "shipping"]]
-    (with-open [writer (io/writer "graphics_cards.csv")]
-      (csv/write-csv writer
-                     (cons headers
-                           (for [e elems
-                                 :let [product-name (str/replace (.text (get-elems e ".item-title")) "," "|")
-                                       brand (.attr (get-elems e "div.item-branding > a > img") "title")
-                                       shipping (-> (get-elems e "div.item-action ul.price > li.price-ship")
-                                                    .text
-                                                    (str/replace "$" "")
-                                                    (str/replace "Shipping" ""))]]
-                             (conj (vector brand) product-name shipping)))))))
+  (generate-csv "graphics_cards.csv" site-url))
+
+(comment
+  (def html (fetch-page site-url))
+
+  (def items (.select html "div.item-container"))
+
+  (.select (first items) ".item-title")
+
+  (.attr (.select (first items) ".item-branding > a > img") "title")
+
+  (parse-product (first items))
+
+  (generate-csv "graphics_cards.csv" site-url))
